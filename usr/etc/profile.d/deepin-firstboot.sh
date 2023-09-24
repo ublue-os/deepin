fb1bb12ca8ebf181f9b697337278228dcbc6a7ba
tulili <tulilirockz.pub+git@gmail.com>
2023-09-24 14:19:12 -0300

    chore: remove firstboot related files for compliance w other images

--- usr/etc/profile.d/deepin-firstboot.sh
+++
@@ -1,6 +0,0 @@
-if test "$(id -u)" -gt "0" && test -d "$HOME"; then
-    if test ! -e "$HOME"/.config/autostart/deepin-firstboot.desktop; then
-        mkdir -p "$HOME"/.config/autostart
-        cp -f /usr/share/ublue-os/firstboot/deepin-firstboot.desktop "$HOME"/.config/autostart
-    fi
-fi
